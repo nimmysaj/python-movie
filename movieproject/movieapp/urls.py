862d639e6850c20682ef4da87d96c1764999c20b
sreedhini k <140672690+sreedhni@users.noreply.github.com>
2024-09-19 19:56:14 +0530

    Update urls.py

--- movieproject/movieapp/urls.py
+++ movieproject/movieapp/urls.py
@@ -2,6 +2,9 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
+
+
+    
     path('',views.index,name='index'),
     path('movie/<int:movie_id>/',views.detail,name='detail'),
     path('add/',views.add_movie,name='add_movie'),
